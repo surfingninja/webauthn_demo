1fe9f1588d0ef1f627245f99ee8093fad2d87862
Viktor Pokhilko <sataninja@laptop.local>
2022-12-12 21:26:53 +0200

    all spec run

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -81,7 +81,7 @@ export const config: Options.Testrunner = {
     // will be called from there.
     //
     specs: [
-        './test/features/**/failauth.feature'
+        './test/features/**/*.feature'
     ],
     // Patterns to exclude.
     exclude: [
