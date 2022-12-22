9209c88421378861e136985387c5c0aca194ce3e
Viktor Pokhilko <sataninja@laptop.local>
2022-12-22 13:48:35 +0200

    small remarks

--- wait_for_grid.sh
+++ wait_for_grid.sh
@@ -8,7 +8,7 @@ cmd="$@"
 while ! curl -sSL "http://localhost:4444/wd/hub/status" 2>&1 \
         | jq -r '.value.ready' 2>&1 | grep "true" >/dev/null; do
     echo 'Waiting for the Grid'
-    sleep 1
+    sleep 2
 done
 
 >&2 echo "Selenium Grid is up - executing tests"
